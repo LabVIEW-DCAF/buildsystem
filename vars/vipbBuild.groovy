3a78711a6bb0fa06490e4d7c2e30a36a567e09e2
DCAF-Builder <mpollock@ni.com>
2017-01-12 14:12:36 -0600

    Create vipbBuild.groovy

---
+++ vars/vipbBuild.groovy
@@ -0,0 +1,13 @@
+def call(path, lvVersion) {
+        echo 'Build the package'
+        
+        def vipb_json = JsonOutput.toJson(['VIPB Path': "${path}","Install after build": false, "LabVIEW Version":"${lvVersion}", Executor_Number: env.EXECUTOR_NUMBER.toString(), Workspace_Path : env.WORKSPACE+'\\build_temp'])
+        echo vipb_json
+        
+        def vipb_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIPB_Build?JSON="+java.net.URLEncoder.encode(vipb_json, "UTF-8").replaceAll("\\+", "%20")
+        println("Status: "+vipb_response.status)
+        println("Content: "+vipb_response.content)
+        
+        echo 'Magic wait of 5 seconds...'
+        sleep(5)
+}
