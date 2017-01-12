8d7cda177605f5ba62aa74243f2e30659c1269a2
DCAF-Builder <mpollock@ni.com>
2017-01-12 15:53:56 -0600

    Update vipbBuild.groovy
    Made vipbPath relative to workspace root

--- vars/vipbBuild.groovy
+++ vars/vipbBuild.groovy
@@ -1,7 +1,7 @@
 def call(path, lvVersion) {
         echo 'Build the package'
         
-        def vipb_json = JsonOutput.toJson(['VIPB Path': "${path}","Install after build": false, "LabVIEW Version":"${lvVersion}", Executor_Number: env.EXECUTOR_NUMBER.toString(), Workspace_Path : env.WORKSPACE+'\\build_temp'])
+        def vipb_json = JsonOutput.toJson(['VIPB Path': "${WORKSPACE}\\${path}","Install after build": false, "LabVIEW Version":"${lvVersion}", Executor_Number: env.EXECUTOR_NUMBER.toString(), Workspace_Path : env.WORKSPACE+'\\build_temp'])
         echo vipb_json
         
         def vipb_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIPB_Build?JSON="+java.net.URLEncoder.encode(vipb_json, "UTF-8").replaceAll("\\+", "%20")
