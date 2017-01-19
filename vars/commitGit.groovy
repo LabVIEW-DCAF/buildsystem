e209ed5b29418f0ce4fd1c64cf4ddfd0b55997f4
pollockm <matthew.pollock@ni.com>
2017-01-19 13:19:20 -0600

    Update commitGit.groovy

--- vars/commitGit.groovy
+++ vars/commitGit.groovy
@@ -1,5 +1,7 @@
 def call(vipbPath){
   echo 'Commit only the VIPB such that the build number is correctly updated between builds, even if the build machine is lost'
+  bat 'git config user.name'
+  bat 'git config user.email'
   bat 'git config --global user.email "mpollock@ni.com"'
   bat 'git config --global user.name "DCAF Build Server"'
   bat 'git commit -m "Auto-update files from build" '+'"'+vipbPath+'"'
