0f148b2d863a9f1ffb2b2b106da161a7c9aebe8f
pollockm <matthew.pollock@ni.com>
2017-01-19 14:07:29 -0600

    Update commitGit.groovy

--- vars/commitGit.groovy
+++ vars/commitGit.groovy
@@ -1,9 +1,12 @@
 def call(vipbPath){
   echo 'Commit only the VIPB such that the build number is correctly updated between builds, even if the build machine is lost'
-  bat 'git config user.name'
-  bat 'git config user.email'
-  bat 'git config --global user.email "mpollock@ni.com"'
-  bat 'git config --global user.name "DCAF Build Server"'
-  bat 'git commit -m "Auto-update files from build" '+'"'+vipbPath+'"'
-  bat 'git push origin HEAD:master'
+  
+ withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'MyID', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
+    bat 'git config user.name'
+    bat 'git config user.email'
+    bat 'git config --global user.email "mpollock@ni.com"'
+    bat 'git config --global user.name "DCAF Build Server"'
+    bat 'git commit -m "Auto-update files from build" '+'"'+vipbPath+'"'
+   bat "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL} HEAD:master"
+ }
 }
