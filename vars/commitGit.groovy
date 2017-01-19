91b62e4ab043c4cef38b5f95bbfdc553e8b13e92
pollockm <matthew.pollock@ni.com>
2017-01-19 10:51:27 -0600

    Update commitGit.groovy

--- vars/commitGit.groovy
+++ vars/commitGit.groovy
@@ -1,4 +1,6 @@
 def call(){
   echo 'Commit any changed files, but do not add any new files'
+  bat 'git config --global user.email "mpollock@ni.com"'
+  bat 'git config --global user.name "DCAF Build Server"'
   bat 'git commit -am "Auto-update files from build"'
 }
