5ed849a024612aa3d8901ef1fbd9ff9c3a17e8c4
pollockm <matthew.pollock@ni.com>
2017-01-19 14:26:30 -0600

    Update commitGit.groovy

--- vars/commitGit.groovy
+++ vars/commitGit.groovy
@@ -4,10 +4,10 @@ def call(vipbPath){
  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'DCAF-Builder', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
     bat 'git config user.name'
     bat 'git config user.email'
-   echo env.GIT_BRANCH
+    echo env.GIT_BRANCH
     bat 'git config --global user.email "mpollock@ni.com"'
     bat 'git config --global user.name "DCAF Build Server"'
     bat 'git commit -m "Auto-update files from build" '+'"'+vipbPath+'"'
-   bat "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${GIT_URL} HEAD:master"
+   bat "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${env.GIT_URL} HEAD:master"
  }
 }
