1d083ad98878064f4f1081aece8415af42d9bbd3
DCAF-Builder <mpollock@ni.com>
2017-01-20 16:24:10 -0600

    Update commitGit.groovy

--- vars/commitGit.groovy
+++ vars/commitGit.groovy
@@ -10,7 +10,7 @@ def call(vipbPath){
    // bat 'git config --global user.name "DCAF Build Server"'
     bat 'git commit -m "Auto-update files from build" '+'"'+vipbPath+'"'
     def git_remote_url=bat 'git remote get-url origin'
-   git_remote_url=git_remote_url-'https://'
+   git_remote_url=git_remote_url.drop(8)    //Drop first 8 characters, which in our case are the https:// chars.  This might break if we change access modes.
    bat "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${git_remote_url} HEAD:master"
    //Note: need to replace master in above line with actual branch.  Currently git environment variables are not populated in pipeline scripts, so need to find another way from command line.
  }
