98253cb7894b8a0598171af646d71c169df0b7f5
pollockm <matthew.pollock@ni.com>
2017-04-12 15:29:16 -0500

    Update commitGit.groovy

--- vars/commitGit.groovy
+++ vars/commitGit.groovy
@@ -4,12 +4,6 @@ def call(vipbPath){
  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'DCAF-Builder', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
     bat 'git config user.name'
     bat 'git config user.email'
-    
-   echo '-------------------'
-   bat 'git rev-parse --abbrev-ref HEAD'
-    git_branch = bat returnStdout: true, script: 'git rev-parse --abbrev-ref HEAD'.trim()
-    echo git_branch
-   echo '--------------------'
    //These lines are commented out because they were only needed once to set up the build environment.  They needed to be called from script because script runs as a different user, and I couldn't be arsed to find the right spot for it globally for all users.
    // bat 'git config --global user.email "mpollock@ni.com"'
    // bat 'git config --global user.name "DCAF Build Server"'
@@ -18,7 +12,6 @@ def call(vipbPath){
    echo "Remote_URL raw: "+git_remote_url
    git_remote_url=git_remote_url.trim()     //get rid of any trailing end of line characters
    git_remote_url=git_remote_url.drop(8)    //Drop first 8 characters, which in our case are the https:// chars.  This might break if we change access modes.
-   bat "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${git_remote_url} HEAD:${git_branch}"
-   //Note: need to replace master in above line with actual branch.  Currently git environment variables are not populated in pipeline scripts, so need to find another way from command line.
+   bat "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${git_remote_url} HEAD:${env.JOB_NAME.replaceFirst('.+/', '')}"
  }
 }
