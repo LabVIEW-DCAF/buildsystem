c9c920e28e8f9602876a4f7f419bef7c1cc07e7c
pollockm <matthew.pollock@ni.com>
2017-04-13 17:01:59 -0500

    Update sendEmails.groovy

--- vars/sendEmails.groovy
+++ vars/sendEmails.groovy
@@ -1,6 +1,8 @@
 def call(){
   //This is a test to determine if the mailer works correctly by forcing an unstable build.
   echo 'Sending emails out if build failed'
+  echo 'Here is the reported changeset of the build:'
+  echo currentBuild.rawBuild.changeset
   currentBuild.result = 'FAILURE'
   step([$class: 'Mailer', notifyEveryUnstableBuild: true, recipients: emailextrecipients([[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']])])
 }
