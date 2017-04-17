9a82319d75c0b9d711ae849e303ef70ad8db0278
pollockm <matthew.pollock@ni.com>
2017-04-17 15:21:05 -0500

    Update checkCommits.groovy

--- vars/checkCommits.groovy
+++ vars/checkCommits.groovy
@@ -1,6 +1,7 @@
 def call(){
     echo 'Checking for commits that were not made by the build system' 
     MAX_MSG_LEN = 100
+    def internalChangesOnly = true
     def changeString = ""
 
     echo "Gathering SCM changes"
@@ -10,12 +11,16 @@ def call(){
         for (int j = 0; j < entries.length; j++) {
             def entry = entries[j]
             truncated_msg = entry.msg.take(MAX_MSG_LEN)
+            if truncated_msg != "Auto-update files from build, ignore this commit"{
+                internalChangesOnly=false
+            }
             changeString += " - ${truncated_msg} [${entry.author}]\n"
         }
     }
 
     if (!changeString) {
         changeString = " - No new changes"
+        internalChangesOnly = false
     }
     echo changeString
 }
