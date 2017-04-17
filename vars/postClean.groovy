1e72808f6657db1baa796329b645a7e9613a7445
pollockm <matthew.pollock@ni.com>
2017-04-17 14:30:18 -0500

    Update postClean.groovy

--- vars/postClean.groovy
+++ vars/postClean.groovy
@@ -1,6 +1,6 @@
 def call(){
     echo 'Archiving all artifacts in the build-temp directory'
-    //archiveArtifacts artifacts: 'build-temp/**'
+    archiveArtifacts artifacts: 'build-temp/**'
     echo 'Cleaning out workspace directory'
     deleteDir()
 }
