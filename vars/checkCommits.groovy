34715c9078f7158117789db60881bdc5c47c701a
pollockm <matthew.pollock@ni.com>
2017-04-17 14:58:40 -0500

    Update checkCommits.groovy

--- vars/checkCommits.groovy
+++ vars/checkCommits.groovy
@@ -1,4 +1,21 @@
 def call(){
     echo 'Checking for commits that were not made by the build system' 
-    echo 'Function currently stubbed'
+    MAX_MSG_LEN = 100
+    def changeString = ""
+
+    echo "Gathering SCM changes"
+    def changeLogSets = currentBuild.rawBuild.changeSets
+    for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            truncated_msg = entry.msg.take(MAX_MSG_LEN)
+            changeString += " - ${truncated_msg} [${entry.author}]\n"
+        }
+    }
+
+    if (!changeString) {
+        changeString = " - No new changes"
+    }
+    echo changeString
 }
