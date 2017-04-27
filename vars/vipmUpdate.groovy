fc0cca5e2ed1ee9f67a7b40c759d875993233f61
DCAF-Builder <mpollock@ni.com>
2017-04-27 16:36:09 -0500

    Update vipmUpdate.groovy

--- vars/vipmUpdate.groovy
+++ vars/vipmUpdate.groovy
@@ -9,12 +9,14 @@ def call(lvVersion) {
 
         echo vipm_update_json       
 
-        def vipm_update_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIPM_UPDATE?JSON="+java.net.URLEncoder.encode(vipm_update_json, "UTF-8").replaceAll("\\+", "%20")
+        def vipm_update_response = httpRequest httpRequest validResponseCodes: "200,500", url:"http://localhost:8002/LabVIEWCIService/VIPM_UPDATE?JSON="+java.net.URLEncoder.encode(vipm_update_json, "UTF-8").replaceAll("\\+", "%20")
 
         println("Status: "+vipm_update_response.status)
 
         println("Content: "+vipm_update_response.content)       
-
+            if (vipm_update_response.status!=200){
+                error("Call to CI Server method VIPM_UPDATE failed with error: "+vipm_update_response.content)
+            }
         echo 'Magic wait of 5 seconds...'
 
         sleep(5)
