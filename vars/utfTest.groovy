ef1f9bbd0e8bb92615b7404d3e90ab588c23fd45
DCAF-Builder <mpollock@ni.com>
2017-04-27 16:25:03 -0500

    Update utfTest.groovy

--- vars/utfTest.groovy
+++ vars/utfTest.groovy
@@ -5,10 +5,12 @@ def call(path){
         def utf_json = JsonOutput.toJson(['Project Path': "${WORKSPACE}\\${path}", Executor_Number: env.EXECUTOR_NUMBER.toString(), 'Workspace_Path': env.WORKSPACE+'\\build_temp'])
         echo utf_json
         
-        def utf_response = httpRequest "http://localhost:8002/LabVIEWCIService/Run_UTF?JSON="+java.net.URLEncoder.encode(utf_json, "UTF-8").replaceAll("\\+", "%20")
+        def utf_response = httpRequest validResponseCodes: "200,500", url:"http://localhost:8002/LabVIEWCIService/Run_UTF?JSON="+java.net.URLEncoder.encode(utf_json, "UTF-8").replaceAll("\\+", "%20")
         println("Status: "+utf_response.status)
         println("Content: "+utf_response.content)
-        
+        if (vipb_response.status!=200){
+                error("Call to CI Server method Run_UTF failed with LabVIEW error: "+vipb_response.content)
+            }
         echo 'Junit'
         junit allowEmptyResults: true, testResults: 'build_temp\\utf_results.xml'
 
