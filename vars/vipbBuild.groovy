8519145bc600559d08c6bedd9cd62ea88748bf33
DCAF-Builder <mpollock@ni.com>
2017-04-27 16:26:01 -0500

    Update vipbBuild.groovy

--- vars/vipbBuild.groovy
+++ vars/vipbBuild.groovy
@@ -6,10 +6,12 @@ def call(path, lvVersion) {
         def vipb_json = JsonOutput.toJson(['VIPB Path': "${WORKSPACE}\\${path}","Install after build": false, "LabVIEW Version":"${lvVersion}", Executor_Number: env.EXECUTOR_NUMBER.toString(), Workspace_Path : env.WORKSPACE+'\\build_temp'])
         echo vipb_json
         
-        def vipb_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIPB_Build?JSON="+java.net.URLEncoder.encode(vipb_json, "UTF-8").replaceAll("\\+", "%20")
+        def vipb_response = httpRequest validResponseCodes: "200,500", url: "http://localhost:8002/LabVIEWCIService/VIPB_Build?JSON="+java.net.URLEncoder.encode(vipb_json, "UTF-8").replaceAll("\\+", "%20")
         println("Status: "+vipb_response.status)
         println("Content: "+vipb_response.content)
-        
+        if (vipb_response.status!=200){
+                error("Call to CI Server method VIPB_Build failed with error: "+vipb_response.content)
+            }
         echo 'Magic wait of 5 seconds...'
         sleep(5)
 }
