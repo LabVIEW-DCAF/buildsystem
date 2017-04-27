b448eeab0f8cf7fbd952fb9159235a0e3fde6680
DCAF-Builder <mpollock@ni.com>
2017-04-27 16:28:35 -0500

    Update vipInstall.groovy

--- vars/vipInstall.groovy
+++ vars/vipInstall.groovy
@@ -9,12 +9,14 @@ def call(vipName, lvVersion) {
 
         echo vip_install_json       
 
-        def vip_install_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIP_INSTALL?JSON="+java.net.URLEncoder.encode(vip_install_json, "UTF-8").replaceAll("\\+", "%20")
+        def vip_install_response = httpRequest validResponseCodes: "200,500", url: "http://localhost:8002/LabVIEWCIService/VIP_INSTALL?JSON="+java.net.URLEncoder.encode(vip_install_json, "UTF-8").replaceAll("\\+", "%20")
 
         println("Status: "+vip_install_response.status)
 
         println("Content: "+vip_install_response.content)       
-
+            if (vip_install_response.status!=200){
+                error("Call to CI Server method VIP_INSTALL failed with error: "+vip_response.content)
+            }
         echo 'Magic wait of 5 seconds...'
 
         sleep(5)
