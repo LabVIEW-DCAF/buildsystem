12abbe7fc8b353c30dbb2da9e8f4c5de6720bf37
DCAF-Builder <mpollock@ni.com>
2017-04-27 16:35:07 -0500

    Update vipUninstall.groovy

--- vars/vipUninstall.groovy
+++ vars/vipUninstall.groovy
@@ -9,12 +9,14 @@ def call(vipName, lvVersion) {
 
         echo vip_uninstall_json       
 
-        def vip_uninstall_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIP_UNINSTALL?JSON="+java.net.URLEncoder.encode(vip_uninstall_json, "UTF-8").replaceAll("\\+", "%20")
+        def vip_uninstall_response = httpRequest validResponseCodes: "200,500", url:"http://localhost:8002/LabVIEWCIService/VIP_UNINSTALL?JSON="+java.net.URLEncoder.encode(vip_uninstall_json, "UTF-8").replaceAll("\\+", "%20")
 
         println("Status: "+vip_uninstall_response.status)
 
         println("Content: "+vip_uninstall_response.content)       
-
+            if (vipb_uninstall_response.status!=200){
+                error("Call to CI Server method VIP_UNINSTALL failed with error: "+vipb_uninstall_response.content)
+            }
         echo 'Magic wait of 5 seconds...'
 
         sleep(5)
