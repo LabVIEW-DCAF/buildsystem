4ec448328cff78bda33a2aafb9ec3d21f14c21a2
DCAF-Builder <mpollock@ni.com>
2017-04-27 16:37:11 -0500

    Update vipUninstall.groovy

--- vars/vipUninstall.groovy
+++ vars/vipUninstall.groovy
@@ -14,8 +14,8 @@ def call(vipName, lvVersion) {
         println("Status: "+vip_uninstall_response.status)
 
         println("Content: "+vip_uninstall_response.content)       
-            if (vipb_uninstall_response.status!=200){
-                error("Call to CI Server method VIP_UNINSTALL failed with error: "+vipb_uninstall_response.content)
+            if (vip_uninstall_response.status!=200){
+                error("Call to CI Server method VIP_UNINSTALL failed with error: "+vip_uninstall_response.content)
             }
         echo 'Magic wait of 5 seconds...'
 
