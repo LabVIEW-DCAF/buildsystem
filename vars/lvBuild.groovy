02d8745c7ed88cd836d95a2661aa84d26081e893
pollockm <matthew.pollock@ni.com>
2017-05-16 14:20:17 -0500

    Fixed quotes around vi path

--- vars/lvBuild.groovy
+++ vars/lvBuild.groovy
@@ -1,5 +1,5 @@
 def call(path, target_name, spec_name, lv_version) {
         echo 'Build the build spec: ${spec_name} under target ${target_name} in project at ${path}'
         
-        bat 'labview-cli --kill --lv-ver ${lv_version} L:\lv_build.vi -- ${path} ${target_name} ${spec_name}'
+        bat 'labview-cli --kill --lv-ver ${lv_version} "L:\lv-build.vi" -- ${path} ${target_name} ${spec_name}'
 }
