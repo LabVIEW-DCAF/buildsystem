c7e5611f566a4144b03b056efaedb11f40ce16c3
pollockm <matthew.pollock@ni.com>
2017-05-15 21:06:16 -0500

    Update utfTest.groovy

--- vars/utfTest.groovy
+++ vars/utfTest.groovy
@@ -1,21 +1,5 @@
-import groovy.json.JsonOutput
-
-def call(path){
+def call(path, lv_versison){
         echo 'Run basic tests before build'
-        def utf_json = JsonOutput.toJson(['Project Path': "${WORKSPACE}\\${path}", Executor_Number: env.EXECUTOR_NUMBER.toString(), 'Workspace_Path': env.WORKSPACE+'\\build_temp'])
-        echo utf_json
-        
-        def utf_response = httpRequest validResponseCodes: "200,500", url:"http://localhost:8002/LabVIEWCIService/Run_UTF?JSON="+java.net.URLEncoder.encode(utf_json, "UTF-8").replaceAll("\\+", "%20")
-        println("Status: "+utf_response.status)
-        println("Content: "+utf_response.content)
-        if (utf_response.status!=200){
-                error("Call to CI Server method Run_UTF failed with LabVIEW error: "+utf_response.content)
-            }
-        echo 'Junit'
-        junit allowEmptyResults: true, testResults: 'build_temp\\utf_results.xml'
-
-        echo 'Magic delay of 5 seconds to let LabVIEW breathe'
-        sleep(5)
-        echo 'Done waiting.'
+        bat 'labview-cli --kill --lv-ver ${lv_version} L:\run_utf.vi -- ${path}'
 }        
        
