127a7cda1c7f6acf8897efb39f6f4e29df0961d6
pollockm <matthew.pollock@ni.com>
2017-05-16 14:56:37 -0500

    Added worskpace argument

--- vars/utfTest.groovy
+++ vars/utfTest.groovy
@@ -1,5 +1,4 @@
 def call(path, lv_versison){
         echo 'Run basic tests before build'
-        bat 'labview-cli --kill --lv-ver ${lv_version} "L:\run-utf.vi" -- ${path}'
+        bat 'labview-cli --kill --lv-ver ${lv_version} "L:\run-utf.vi" -- ${path} "build_temp\\utf_results.xml ${WORKSPACE}" '
 }        
-       
