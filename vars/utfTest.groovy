3a9c77c486ece34d47bfc39075fffd98dab6bbb7
pollockm <matthew.pollock@ni.com>
2017-05-16 14:21:41 -0500

    Escape path

--- vars/utfTest.groovy
+++ vars/utfTest.groovy
@@ -1,5 +1,5 @@
 def call(path, lv_versison){
         echo 'Run basic tests before build'
-        bat 'labview-cli --kill --lv-ver ${lv_version} L:\run_utf.vi -- ${path}'
+        bat 'labview-cli --kill --lv-ver ${lv_version} "L:\run-utf.vi" -- ${path}'
 }        
        
