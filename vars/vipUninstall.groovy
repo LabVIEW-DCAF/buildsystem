a1ee4927a45c38364b5bf9b8df92cd78e2a4dced
pollockm <matthew.pollock@ni.com>
2017-05-15 21:58:35 -0500

    Update vipUninstall.groovy

--- vars/vipUninstall.groovy
+++ vars/vipUninstall.groovy
@@ -1,4 +1,4 @@
 def call(vipName, lvVersion) {
         echo 'Uninstall the package '+ vipName
-        bat 'labview-cli --kill --lv-ver ${lvVersion} L:\vipUninstall -- ${vipName}'
+        bat 'labview-cli --kill --lv-ver ${lvVersion} L:\vipUninstall -- ${vipName} ${lvVersion}'
 }
