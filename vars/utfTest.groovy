62c7b3eaf869066ab263a7e001117e46ef9014b6
pollockm <matthew.pollock@ni.com>
2017-05-16 16:48:49 -0500

    Update utfTest.groovy

--- vars/utfTest.groovy
+++ vars/utfTest.groovy
@@ -1,4 +1,4 @@
 def call(path, lv_versison){
         echo 'Run basic tests before build'
-        bat 'labview-cli --kill --lv-ver ${lv_version} "L:\\run-utf.vi" -- ${path} "build_temp\\utf_results.xml" ${WORKSPACE}" '
+        bat "labview-cli --kill --lv-ver ${lv_version} 'L:\\run-utf.vi' -- ${path} 'build_temp\\utf_results.xml' ${WORKSPACE}"
 }        
