1ce9d6bc639b096564279464c1224707f3057b33
pollockm <matthew.pollock@ni.com>
2017-05-17 15:26:08 -0500

    Update lvBuild.groovy

--- vars/lvBuild.groovy
+++ vars/lvBuild.groovy
@@ -1,4 +1,4 @@
 def call(path, target_name, spec_name, lv_version) {
         echo "Build the build spec: ${spec_name} under target ${target_name} in project at ${path}"
-        bat "labview-cli --kill --lv-ver ${lv_version} \"L:\\lv-build.vi\" -- \"${path}\" \"${target_name}\" \"${spec_name}\" \"${WORKSPACE}\""
+        bat "labview-cli --kill --lv-ver ${lv_version} \"L:\\lv-build.vi\" -- \"${path}\" \"${target_name}\" \"${spec_name}\" \"\\build_temp\" \"${WORKSPACE}\""
 }
