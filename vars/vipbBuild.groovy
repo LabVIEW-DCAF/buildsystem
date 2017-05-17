b39340dd4ae4fdbaac82751f1313648d8c491160
pollockm <matthew.pollock@ni.com>
2017-05-17 12:20:13 -0500

    Update vipbBuild.groovy

--- vars/vipbBuild.groovy
+++ vars/vipbBuild.groovy
@@ -2,4 +2,5 @@ def call(path, lvVersion) {
         echo "Build the package at ${path}"
         //bat "labview-cli --kill --lv-ver ${lvVersion} \"L:\\vipbBuild.vi\" -- \"${path}\" \"${WORKSPACE}\\build_temp\""  
         bat '"C:\Program Files (x86)\National Instruments\LabVIEW 2014\labview.exe "C:\Users\Benjamin\Desktop\build.vi"'
+        sleep(240)
 }
