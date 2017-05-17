66f668ac7b2a96aacfee807bf25778cd7c3a7ac8
pollockm <matthew.pollock@ni.com>
2017-05-17 15:22:23 -0500

    Update utfTest.groovy

--- vars/utfTest.groovy
+++ vars/utfTest.groovy
@@ -1,4 +1,6 @@
 def call(path, lv_version){
         echo 'Run basic tests before build'
         bat "labview-cli --kill --lv-ver ${lv_version} L:\\run-utf.vi -- \"${path}\" \"build_temp\\utf_results.xml\" \"${WORKSPACE}\""
+        echo 'Junit'
+        junit allowEmptyResults: true, testResults: 'build_temp\\utf_results.xml'
 }        
