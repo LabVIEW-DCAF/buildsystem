5bd3c6cc2d9b6d04670fd429979c57d1ac202eed
theSloopJohnB <john.boyd@ni.com>
2017-08-24 09:57:29 -0500

    diff incoming PRs and post to github
    This change will insert a step into our build pipeline to automatically
    diff VIs against the master version to make reviews easier.
    
    It executes the following steps:
    
    1.  Check if this is a pull request (via env.CHANGE_ID)
    2. Check if diffing is enabled (via env.DIFFING_ENABLED, a custom
    variable set on Jenkins global config, it can be set to anything.)
    3. Clean up the workspace's diff directory.
    4. Prompt Git to launch a custom difftool - 'labview.bat'. labview.bat
    will call lvDiff.vi on the VIs to produce pretty pictures.
    5. Call python scripts to post output to GitHub PR.
    
    This change adds a few dependencies to work correctly:
    
    1. Python 3.
    2. Environment variables, DIFFING_ENABLED, mentioned above, and
    GITHUB_DIFF_TOKEN, a token generated from Github which lets the code
    post to Github.
    3. lvrevbrd. This is a set of internal to NI tools that lvDiff calls to
    do its magic. Hopefully a future change will be able to open source them
    and use them as a project.

---
+++ steps/post_pics_to_pr.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import base64
+import os
+import datetime
+import logging
+
+_moduleLogger = logging.getLogger(__name__)
+
+def _create_header(token):
+    return {'Authorization': 'token %s' % token.strip()}
+
+
+def _read_picture(file_name):
+    with open(file_name, 'rb') as text:
+        return base64.b64encode(text.read()).decode()
+
+
+def _post_file(file_data, folder, file_name, header):
+    # put encoded data into json request
+    new_file_data = json.dumps({"message": "commit message", "content":file_data})
+
+    # post a picture to a repo
+    url = 'https://api.github.com/repos/theSloopJohnB/thesloopjohnb/contents/%s/%s' % (folder, file_name)
+    
+    r=requests.put(url, data=new_file_data, headers=header)
+    if (r.ok):
+        _moduleLogger.info('Response code: %s', r.status_code)
+    else:
+        _moduleLogger.error('Bad response code: %s', r.status_code)
+        _moduleLogger.error('Bad response text: %s', r.text)
+    return r.json()['content']['download_url']
+
+
+# post a comment on an issue
+def _post_comment_to_pr(urlPicPairs, pullRequestInfo, prNumber, header):
+    formatString = "### %s: ![capture](%s)\n\n"
+    body = """Bleep bloop!
+
+LabVIEW Diff Robot here with some diffs served up hot for your pull request.
+
+Notice something funny? Help fix me on [my GitHub repo.](https://github.com/theSloopJohnB/LabVIEW-Diff)
+
+
+"""
+    for pair in urlPicPairs:
+        body += formatString % pair
+
+    org, repo, _ = pullRequestInfo.split('/')
+    url = "https://api.github.com/repos/%s/%s/issues/%s" % (org, repo, prNumber)
+    data = json.dumps({"body":body})
+    r = requests.post(url, data=data, headers=header)
+    if (r.ok):
+        _moduleLogger.info('Response code: %s', r.status_code)
+    else:
+        _moduleLogger.error('Bad response code: %s', r.status_code)
+        _moduleLogger.error('Bad response text: %s', r.text)
+
+def post_pics_to_pr(token, localPicfileDirectory, pullRequestInfo, prNumber):
+    header = _create_header(token)
+    pics = [f for f in os.listdir(localPicfileDirectory) if f.endswith(".png")]
+    folder = pullRequestInfo + '/' + datetime.datetime.now().strftime('%Y-%m-%d/%H:%M:%S')
+    picUrls = []
+    for pic in pics:
+        picData = _read_picture(os.path.join(localPicfileDirectory, pic))
+        picUrl = _post_file(picData, folder, os.path.split(pic)[1], header)
+        picUrls.append((pic, picUrl))
+
+    _post_comment_to_pr(picUrls, pullRequestInfo, prNumber, header) 
