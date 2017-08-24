e4054e5b3111c51363cab947b44e28d1435b7730
theSloopJohnB <john.boyd@ni.com>
2017-08-24 13:38:19 -0500

    fixing path to python file

--- vars/dcafPipeline.groovy
+++ vars/dcafPipeline.groovy
@@ -44,7 +44,7 @@ def continueBuild
           bat "mkdir ${diffDir}"
           bat "git difftool --no-prompt --extcmd=\"'L:\\labview.bat' \$LOCAL \$REMOTE diff_dir ${lvVersion}\" origin/master HEAD"
           // Silencing echo so as to not print out the token.
-          bat "@python C:\\Users\\nitest\\Documents\\GitHub\\LabVIEW-Diff\\github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME}"
+          bat "@python L:\\github_commenter.py --token=${GITHUB_DIFF_TOKEN} --pic-dir=${diffDir} --pull-req=${CHANGE_ID} --info=${JOB_NAME}"
         }
       }
       stage ('Check Preconditions for Build'){
