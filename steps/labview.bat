cfeefaa3a4f57f482c32bb25a621c96f591e51da
theSloopJohnB <john.boyd@ni.com>
2017-08-28 10:10:31 -0500

    making it work with newly added VIs

--- steps/labview.bat
+++ steps/labview.bat
@@ -1,15 +1,15 @@
-@SET vi1=%~f1
-@SET vi2=%~f2
+SET vi1=%~f1
+SET vi2=%~f2
 @SET working_dir=%~f3
 @SET lv_version=%4
 
-@REM only run if both files are VIs.
+@REM only run diff if both files are VIs.
 
-@if "%vi1:~-3%" == ".vi" (
+@if "%vi1:~-3%" == ".vi" OR "%vi1:~-3%" == "\\.\nul" (
     if "%vi2:~-3%" == ".vi" GOTO :DIFF_VI
 )
 GOTO :END
 
 :DIFF_VI
-    labview-cli --kill --lv-ver %lv_version% L:\lvDiff.vi -- "%vi1%" "%vi2%" "%working_dir%"
+    @echo labview-cli --kill --lv-ver %lv_version% L:\lvDiff.vi -- "%vi1%" "%vi2%" "%working_dir%"
 :END
