2b3407035d88b3392cee6ee4cf77abc4e0be0bfc
theSloopJohnB <john.boyd@ni.com>
2017-08-28 10:22:28 -0500

    fixing nul check and silencing bat

--- steps/labview.bat
+++ steps/labview.bat
@@ -1,15 +1,17 @@
-SET vi1=%~f1
-SET vi2=%~f2
+@SET vi1=%~f1
+@SET vi2=%~f2
 @SET working_dir=%~f3
 @SET lv_version=%4
 
-@REM only run diff if both files are VIs.
+@REM only run diff if both files are VIs or there was a newly added VI
 
-@if "%vi1:~-3%" == ".vi" OR "%vi1:~-3%" == "\\.\nul" (
-    if "%vi2:~-3%" == ".vi" GOTO :DIFF_VI
-)
-GOTO :END
+@if "%vi1:~-3%" == ".vi" GOTO :VI2Check
+@if "%vi1%" == "\\.\nul" GOTO :VI2Check
+@GOTO :END
+
+:VI2Check
+@if "%vi2:~-3%" == ".vi" GOTO :DIFF_VI
 
 :DIFF_VI
-    @echo labview-cli --kill --lv-ver %lv_version% L:\lvDiff.vi -- "%vi1%" "%vi2%" "%working_dir%"
+    labview-cli --kill --lv-ver %lv_version% L:\lvDiff.vi -- "%vi1%" "%vi2%" "%working_dir%"
 :END
